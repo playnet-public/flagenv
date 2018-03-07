15fc190e3034107b924c64b35e54958667891045
Vincent <agpheaddikilla@yahoo.de>
2018-03-07 20:30:47 +0100

    updated flagenv package

--- flagenv.go
+++ flagenv.go
@@ -11,20 +11,47 @@ import (
 	"github.com/golang/glog"
 )
 
+//EnvPrefix is the prefix for environment variables
+var EnvPrefix string
+
+//String returns a flagenv variable pointer of type string
 func String(name string, value string, usage string) *string {
 	return flag.String(name, envString(parameterNameToEnvName(name), value), usage)
 }
 
+//StringVar sets a given pointer flagenv variable of type string
+func StringVar(p *string, name string, value string, usage string) {
+	flag.StringVar(p, name, envString(parameterNameToEnvName(name), value), usage)
+}
+
+//Duration returns a flagenv variable pointer of type time.Duration
 func Duration(name string, value time.Duration, usage string) *time.Duration {
 	return flag.Duration(name, envDuration(parameterNameToEnvName(name), value), usage)
 }
 
+//DurationVar sets a given pointer flagenv variable of type time.Duration
+func DurationVar(p *time.Duration, name string, value time.Duration, usage string) {
+	flag.DurationVar(p, name, envDuration(parameterNameToEnvName(name), value), usage)
+}
+
+//Int returns a flagenv variable pointer of type int
 func Int(name string, value int, usage string) *int {
 	return flag.Int(name, envInt(parameterNameToEnvName(name), value), usage)
 }
 
+//IntVar sets a given pointer flagenv variable of type int
+func IntVar(p *int, name string, value int, usage string) {
+	flag.IntVar(p, name, envInt(parameterNameToEnvName(name), value), usage)
+}
+
+//Bool returns a flagenv variable pointer of type bool
 func Bool(name string, value bool, usage string) *bool {
 	return flag.Bool(name, envBool(parameterNameToEnvName(name), value), usage)
+}
+
+//BoolVar sets a given pointer flagenv variable of type bool
+func BoolVar(p *bool, name string, value bool, usage string) {
+	flag.BoolVar(p, name, envBool(parameterNameToEnvName(name), value), usage)
 }
 
 func envString(key, def string) string {
@@ -71,9 +98,10 @@ func envInt(key string, def int) int {
 }
 
 func parameterNameToEnvName(name string) string {
-	return strings.Replace(strings.ToUpper(name), "-", "_", -1)
+	return EnvPrefix + strings.Replace(strings.ToUpper(name), "-", "_", -1)
 }
 
+//Parse is an alias to flag.Parse()
 func Parse() {
 	flag.Parse()
 }
